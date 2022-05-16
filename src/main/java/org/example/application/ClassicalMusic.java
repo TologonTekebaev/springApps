50078f4d8bf2426a5f1b76925590a5ddfc930767
Admin <tologon.tkbv@gmail.com>
2022-05-17 04:30:15 +0600

    spring practice

---
+++ src/main/java/org/example/application/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package org.example.application;
+
+public class ClassicalMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Hello";
+    }
+}
---
+++ src/main/java/org/example/application/RockMusic.java
@@ -0,0 +1,8 @@
+package org.example.application;
+
+public class RockMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Waiting for you";
+    }
+}
